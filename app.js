e884ee94ec00018fa82be2227cfa0868e14386c4
tanelson92 <trent@pegasuscart.com>
2022-04-04 15:22:25 -0600

    add final images, styling, and page-not-found pug.

--- app.js
+++ app.js
@@ -9,20 +9,28 @@ app.use(routes);
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-//404 
+/*
+* 404 - Page Not Found
+*/
 app.use((req, res, next) => {
     const err = new Error();
     err.status = 404;
     err.message = 'Oops! Page Not Found.';
-    next(err);
+    res.render('page-not-found', err);
 });
 
-//Errors
+/*
+* Error Catch All Page
+* Catch all other new errors and render "error" for user. 
+*/
 app.use((err, req, res, next) => {
     res.status(err.status);
     res.render('error', err);
 });
 
+/*
+* Express hosting live website. 
+*/
 app.listen(3000, () => {
     console.log('Static Node and Express App - Unit 6 - Running on localhost:3000');
 });
--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,40 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data/projects.json'); 
 
-//Homepage
+/* 
+* Homepage
+*/
+
 router.get('/', (req, res) => {
-    res.locals.projects = data.projects;
-    console.log(res.locals.projects);
-    res.render('index', { projects: res.locals.projects });
+    res.app.locals.projects = data.projects;
+    res.render('index', { projects: res.app.locals.projects });
 });
 
-//Projects
+/* 
+* Project Page
+* Get random project from existing projects. 
+*/
+
 router.get('/project', (req, res) => {
     const randomProject = Math.floor( Math.random() * data.projects.length );
     res.redirect(`/project/${randomProject}`);
 });
 
+/* 
+* Project Page
+* Select a project page based on the "ID" query provided. 
+*/
 router.get('/project/:id', (req, res) => {
-    res.locals.project = data.projects.find( project => { return project.id === parseInt(req.params.id) });
-    res.render('project', res.locals.project );
+    res.locals.project = Object.assign({}, data.projects.find( project => { return project.id === parseInt(req.params.id) }) );
+    res.locals.project.image_urls = res.locals.project.image_urls.slice(0,2); //Remove Landing page image.
+    res.render('project', res.locals.project);
 });
 
-//About
+
+/* 
+* About Page
+*/
+
 router.get('/about', (req, res) => {
     res.render('about');
 });
